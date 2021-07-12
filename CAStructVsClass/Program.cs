76fc670d7f30d25adbcac9615cea69b02c2dd78a
Metigator <metigator@gmail.com>
2021-07-12 11:33:33 -0400

    initial commit

---
+++ CAStructVsClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CAStructVsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 05, 01, 08, 30, 00);
+
+            dt = dt.AddDays(10);
+
+            Console.WriteLine(dt);
+
+            Console.ReadKey();
+        }
+
+         
+    } 
+}
